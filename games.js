c4ff5003f0cf1df6ff7b3a3a41db8ddcd9c37e6b
Andrej Cezner <acezner@gmail.com>
2015-02-05 14:23:58 +0100

    Adding game collection and deleting some packages

---
+++ games.js
@@ -0,0 +1,35 @@
+Games = new Meteor.Collection('games');
+
+/*
+game ={
+	currentTurn = [];
+	deck: [],
+	table:[],
+	players:{
+		a: {
+			hand:[]
+			pile:[]
+			score:[]
+		}
+		b:{}
+	}
+	inProgress: true / false,
+	started: date.
+	finished: data,
+	winner: id
+}
+ */
+if (Meteor.isServer) {
+	Meteor.publish('games', function () {
+		return Games.find({ currentTurn: this.userId });
+	});
+
+	Meteor.publish('users', function () {
+		return Meteor.users.find();
+	});
+}
+
+if (Meteor.isClient) {
+	Meteor.subscribe('games');
+	Meteor.subscribe('users');
+}
